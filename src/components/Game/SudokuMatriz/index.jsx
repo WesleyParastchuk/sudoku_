16f222764fa8cc5432e0e930c9147b75a17b5cb7
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2023-07-04 15:16:09 -0300

    remocao de bug

--- src/components/Game/SudokuMatriz/index.jsx
+++ src/components/Game/SudokuMatriz/index.jsx
@@ -5,7 +5,7 @@ import "./SudokuMatriz.css";
 
 import { game } from "../../../App";
 
-const SudokuMatriz = props => {
+const SudokuMatriz = (props) => {
 	function setFocusLoc(newLoc) {
 		game.cellClicked = newLoc;
 		props.setClickLoc(newLoc);
@@ -18,31 +18,6 @@ const SudokuMatriz = props => {
 		];
 	}
 
-	document.addEventListener("keyup", event => {
-		const pos = game.cellClicked;
-		if (event.key == "ArrowLeft")
-			if (pos[1] > 1) {
-				setFocusLoc([pos[0], pos[1] - 1]);
-				return;
-			}
-		if (event.key == "ArrowRight")
-			if (pos[1] < 8) {
-				setFocusLoc([pos[0], pos[1] + 1]);
-				return;
-			}
-		if (event.key == "ArrowUp")
-			if (pos[0] > 0) {
-				setFocusLoc([pos[0] + 1, pos[1]]);
-				return;
-			}
-		if (event.key == "ArrowDown")
-			if (pos[0] < 8) {
-				setFocusLoc([pos[0] - 1, pos[1]]);
-				return;
-			}
-		return;
-	});
-
 	return (
 		<>
 			{props.thisGame.map((line, row) => {
@@ -51,7 +26,9 @@ const SudokuMatriz = props => {
 						<CellSpace
 							row={row}
 							column={column}
-							onClick={event => setFocusLoc(convertEvent(event))}
+							onClick={(event) =>
+								setFocusLoc(convertEvent(event))
+							}
 							key={`${row} ${column}`}
 							innerHTML={cell}
 							clickLoc={props.clickLoc}
